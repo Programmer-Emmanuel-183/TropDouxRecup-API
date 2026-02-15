79b37431570aea890c5d46bdd31d7aa39c810cf8
Emmanuel Bamidélé <165570846+Programmer-Emmanuel@users.noreply.github.com>
2026-02-15 19:42:45 +0000

    Resoudre bug OTP

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -72,7 +72,7 @@ class AuthController extends Controller
                         'email_marchand' => $request->email,
                         'tel_marchand' => $request->telephone,
                         'id_commune' => $request->id_localite,
-                        'image_marchand' => '"https://i.ibb.co/Qvw3vTpF/3fb662add763.jpg"',
+                        'image_marchand' => 'https://i.ibb.co/Qvw3vTpF/3fb662add763.jpg',
                         'password_marchand' => Hash::make($request->password),
                         'code_otp' => $code_otp,
                         'otp_expire_at' => now()->addMinutes(10),
